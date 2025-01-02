cdeb1f3bd3566f93c7f88a6fba6bc2d39ef8ce25
Aruanaaa <zhaksylykaruana@gmail.com>
2025-01-02 20:16:43 +0500

    MenuItem.java

--- MenuItem.java
+++ MenuItem.java
@@ -1,28 +1,41 @@
 import java.util.Objects;
+
 public class MenuItem {
     private String name;
     private double price;
     private String category;
+
     public MenuItem(String name, double price, String category) {
         this.name = name;
         this.price = price;
         this.category = category;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public double getPrice() {
         return price;
     }
     public void setPrice(double price) {
         this.price = price;
     }
+
+    public static void setPrice(MenuItem[] items, double price) {
+        for (MenuItem item : items) {
+            item.price = price;
+        }
+    }
+
     public String getCategory() {
         return category;
     }
+
     public void setCategory(String category) {
         this.category = category;
     }
@@ -45,6 +58,7 @@ public class MenuItem {
                 Objects.equals(name, menuItem.name) &&
                 Objects.equals(category, menuItem.category);
     }
+
     @Override
     public int hashCode() {
         return Objects.hash(name, price, category);
