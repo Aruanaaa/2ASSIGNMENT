e975a61535cd84c2f6c8821c5626298cf8de92ec
Aruanaaa <zhaksylykaruana@gmail.com>
2025-01-27 12:30:35 +0500

    Restaurant.java

--- Restaurant.java
+++ Restaurant.java
@@ -1,50 +1,21 @@
-package com.restaurant;
-
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Restaurant {
     private String name;
     private String address;
-    private List<MenuItem> menu;
 
     public Restaurant(String name, String address) {
         this.name = name;
         this.address = address;
-        this.menu = new ArrayList<>();
     }
 
-    public void addMenuItem(MenuItem item) {
-        menu.add(item);
+    // Получение меню из базы данных
+    public List<MenuItem> getMenuItemsFromDb() {
+        return RestaurantDatabase.getMenuItems();  // Метод из класса RestaurantDatabase
     }
 
-    public List<MenuItem> filterByCategory(String category) {
-        return menu.stream()
-                .filter(item -> item.getCategory().equalsIgnoreCase(category))
-                .collect(Collectors.toList());
+    // Добавление нового элемента меню
+    public void addMenuItem(MenuItem menuItem) {
+        RestaurantDatabase.addMenuItem(menuItem);
     }
 
-    public List<MenuItem> sortByPrice(boolean ascending) {
-        return menu.stream()
-                .sorted(ascending ? Comparator.comparingDouble(MenuItem::getPrice)
-                        : Comparator.comparingDouble(MenuItem::getPrice).reversed())
-                .collect(Collectors.toList());
-    }
-
-    public List<MenuItem> searchByName(String name) {
-        return menu.stream()
-                .filter(item -> item.getName().toLowerCase().contains(name.toLowerCase()))
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public String toString() {
-        return "Restaurant{" +
-                "name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                ", menu=" + menu +
-                '}';
-    }
+    // И другие методы работы с базой данных...
 }
